3b59f5b684c2c8810bbdb2e92714b8adb8af0241
Christian Kokott <christian.kokott@gmail.com>
2016-12-09 16:24:41 +0100

    Added path argument for show.py

--- Python/show.py
+++ Python/show.py
@@ -4,17 +4,20 @@ import matplotlib.cm as cm
 from matplotlib import colors
 from scipy import misc
 import argparse
+import os
 
 frame = 0
 
 parser = argparse.ArgumentParser(description='Display recorded Kinect data')
+parser.add_argument('-p', dest='recording_path', action='store', type=str, default="", help="Which recording do you want to display?")
 parser.add_argument('-f', dest='frame', action='store', type=int, default=0, help='Which frame do you want to display')
 
 args = parser.parse_args()
 frame = args.frame
+recording_path = args.recording_path
 
 #COLOR
-color = np.fromfile('COLOR/%d.uint8' % frame, dtype=np.uint8)
+color = np.fromfile(os.path.join(recording_path, 'COLOR', '%d.uint8' % frame), dtype=np.uint8)
 color.shape = (1080, 1920, 4)
 plt.subplot(2, 2, 1)
 plt.imshow(color)
@@ -26,7 +29,7 @@ max_depth = 4500 # Max reliable depth
 
 max_value = np.iinfo(np.uint16).max
 
-depth = np.fromfile('DEPTH/%d.uint16' % frame, dtype=np.uint16)
+depth = np.fromfile(os.path.join(recording_path, 'DEPTH', '%d.uint16' % frame), dtype=np.uint16)
 depth.shape = (424, 512)
 
 plt.subplot(2, 2, 2)
@@ -34,7 +37,7 @@ plt.imshow(depth, interpolation='nearest', cmap=cm.gist_heat)
 plt.title('Depth values 512x424')
 
 #INDEX
-index = np.fromfile('INDEX/%d.uint8' % frame, dtype=np.uint8)
+index = np.fromfile(os.path.join(recording_path, 'INDEX', '%d.uint8' % frame), dtype=np.uint8)
 index.shape = (424, 512)
 index[index == 255] = 8
 
@@ -45,7 +48,7 @@ plt.imshow(index, interpolation='nearest', cmap=index_color_map)
 plt.title('Segmentation data 512x424')
 
 #TRACKEDCOLOR
-tracked = np.fromfile('TRACKEDCOLOR/%d.uint8' % frame, dtype=np.uint8);
+tracked = np.fromfile(os.path.join(recording_path, 'TRACKEDCOLOR', '%d.uint8' % frame), dtype=np.uint8);
 tracked.shape = (424, 512, 4)
 plt.subplot(2, 2, 4)
 plt.imshow(tracked)
