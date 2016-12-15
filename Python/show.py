bd9f27cdeb129d0ef71a3ee3de98041c0266ea1b
Christian Kokott <christian.kokott@gmail.com>
2016-12-15 16:47:05 +0100

    Added file checking in show.py

--- Python/show.py
+++ Python/show.py
@@ -16,41 +16,51 @@ frame = args.frame
 recording_path = args.recording_path.strip(os.sep)
 
 #COLOR
-color = np.fromfile(os.path.join(recording_path, 'COLOR', '%d.uint8' % frame), dtype=np.uint8)
-color.shape = (1080, 1920, 4)
-plt.subplot(2, 2, 1)
-plt.imshow(color)
-plt.title('Original color 1920x1080')
+colorFile = os.path.join(recording_path, 'COLOR', '%d.uint8' % frame)
+if (os.path.isfile(colorFile)):
+    color = np.fromfile(colorFile, dtype=np.uint8)
+    color.shape = (1080, 1920, 4)
+    plt.subplot(2, 2, 1)
+    plt.imshow(color)
+    plt.title('Original color 1920x1080')
+
+
 
 #DEPTH
-min_depth = 500  # Min reliable depth
-max_depth = 4500 # Max reliable depth
+depthFile = os.path.join(recording_path, 'DEPTH', '%d.uint16' % frame)
+if (os.path.isfile(depthFile)):
+    min_depth = 500  # Min reliable depth
+    max_depth = 4500 # Max reliable depth
 
-max_value = np.iinfo(np.uint16).max
+    max_value = np.iinfo(np.uint16).max
 
-depth = np.fromfile(os.path.join(recording_path, 'DEPTH', '%d.uint16' % frame), dtype=np.uint16)
-depth.shape = (424, 512)
+    depth = np.fromfile(depthFile, dtype=np.uint16)
+    depth.shape = (424, 512)
 
-plt.subplot(2, 2, 2)
-plt.imshow(depth, interpolation='nearest', cmap=cm.gist_heat)
-plt.title('Depth values 512x424')
+    plt.subplot(2, 2, 2)
+    plt.imshow(depth, interpolation='nearest', cmap=cm.gist_heat)
+    plt.title('Depth values 512x424')
 
 #INDEX
-index = np.fromfile(os.path.join(recording_path, 'INDEX', '%d.uint8' % frame), dtype=np.uint8)
-index.shape = (424, 512)
-index[index == 255] = 8
+indexFile = os.path.join(recording_path, 'INDEX', '%d.uint8' % frame)
+if (os.path.isfile(indexFile)):
+    index = np.fromfile(indexFile, dtype=np.uint8)
+    index.shape = (424, 512)
+    index[index == 255] = 8
 
-index_color_map = colors.ListedColormap(['red', 'green', 'blue', 'cyan', 'magenta', 'yellow', '#ff7700', 'white', 'black'])
+    index_color_map = colors.ListedColormap(['red', 'green', 'blue', 'cyan', 'magenta', 'yellow', '#ff7700', 'white', 'black'])
 
-plt.subplot(2, 2, 3)
-plt.imshow(index, interpolation='nearest', cmap=index_color_map)
-plt.title('Segmentation data 512x424')
+    plt.subplot(2, 2, 3)
+    plt.imshow(index, interpolation='nearest', cmap=index_color_map)
+    plt.title('Segmentation data 512x424')
 
 #TRACKEDCOLOR
-tracked = np.fromfile(os.path.join(recording_path, 'TRACKEDCOLOR', '%d.uint8' % frame), dtype=np.uint8);
-tracked.shape = (424, 512, 4)
-plt.subplot(2, 2, 4)
-plt.imshow(tracked)
-plt.title('Color mapped to tracked depth space 512x424')
+trackedFile = os.path.join(recording_path, 'TRACKEDCOLOR', '%d.uint8' % frame)
+if (os.path.isfile(trackedFile)):
+    tracked = np.fromfile(trackedFile, dtype=np.uint8);
+    tracked.shape = (424, 512, 4)
+    plt.subplot(2, 2, 4)
+    plt.imshow(tracked)
+    pltself.title('Color mapped to tracked depth space 512x424')
 
 plt.show()
